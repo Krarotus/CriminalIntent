a693fd60e04702fbf2f33e6cc3e7d02bfb08d444
Krarotus <kd7479745@gmail.com>
2023-12-29 12:01:12 +0700

    Краши убраны :)

---
+++ app/src/main/java/com/sample/criminalintent/Crime.kt
@@ -0,0 +1,15 @@
+package com.sample.criminalintent
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.UUID
+
+@Entity
+
+data class Crime(
+    @PrimaryKey
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false)
